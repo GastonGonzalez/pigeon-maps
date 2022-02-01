059dddde71023850254dd83f6688dc04c139c3b2
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2022-02-01 04:21:01 -0700

    Added Line component from issue/31 example

--- demo/Demo.tsx
+++ demo/Demo.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import { PigeonIcon } from './PigeonIcon'
-import { Point, Map, Marker, Draggable, ZoomControl } from '../src'
+import { Line, Point, Map, Marker, Draggable, ZoomControl } from '../src'
 import * as providers from '../src/providers'
 
 const markers = {
@@ -169,6 +169,8 @@ export function Demo(): JSX.Element {
             <PigeonIcon width={100} height={95} />
           </Draggable>
           <ZoomControl />
+          <Line coordsArray={[markers.leuven1[0], markers.leuven2[0]]} />
+
         </Map>
       </div>
       <div>
